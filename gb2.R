0e36759dcce86a672ca5b4ce05dd1ed049a76413
yukke <soh.ishiguro3914@gmail.com>
2014-09-24 00:23:03 +0900

    Has a R5 class (currently converting...)

--- gb2.R
+++ gb2.R
@@ -2,27 +2,109 @@ library(Gviz)
 library(Rsamtools)
 library(dplyr)
 
-query.by.symbol <- function(db=NULL, ...) {
-  #if (is.na(db)){
-  #  stop("Error: need DB for building query")
-  #}
-  args = list(...)
-  result <- data.frame()
-  for (i in args){
-    result <- rbind(result, filter(db, symbol == i) %>% select(chromosome, start, end, symbol, transcript))
-  }
-  return(result)
-}
 
-generate.query <- function(db=NULL, query=NULL){
-  three <- gsub("mir", "miR", query) %>% paste("-3p", sep="")
-  five <- gsub("mir", "miR", query) %>% paste("-5p", sep="")
-  queries <- query.by.symbol(db=db, query, three, five)
-  if (nrow(queries) == 0) {
-    stop(sprintf("QueryError: Can not find \"%s\" in db", query))
-  }
-  return(queries)
-}
+Cligb <- setRefClass(
+    Class = "Cligb",
+
+    fields = list(
+        genome.ver = "character",
+        chromosome = "character",
+        start = "numeric",
+        end = "numeric",
+        interest = "data.frame"
+    ),
+
+    methods = list(
+        initialize = function(genome.ver=NULL) {
+            if (is.null(genome.ver)) {
+                warning("Genome version is not specified, set default[hg19]")
+                genome.ver <<- "hg19"
+            } else {
+                genome.ver <<- genome.ver
+            }
+        },
+        
+        generate.db = function(file=NULL) {
+            if (!is.null(file)){
+                df <- read.table(file, sep="\t", header=TRUE)
+                return(df)
+            }
+        },
+
+        query.by.symbol = function(db=NULL, ...) {
+            if (is.null(db)) {
+                stop("DB is not specified!")
+            }
+            args = list(...)
+            result <- data.frame()
+            for (i in args){
+                result <- rbind(result, filter(db, symbol == i) %>% select(chromosome, start, end, strand, symbol, transcript))
+            }
+            result
+        },
+
+        find.mirs = function(db=NULL, query=NULL){
+            three <- gsub("mir", "miR", query) %>% paste("-3p", sep="")
+            five <- gsub("mir", "miR", query) %>% paste("-5p", sep="")
+            queries <- query.by.symbol(db=db, query, three, five)
+            if (nrow(queries) == 0) {
+                stop(sprintf("Query error: Can not find \"%s\" in db", query))
+            }
+            .set.interest(queries)
+            interest <<- queries
+        },
+
+        .set.interest = function(q) {
+            chromosome <<- as.character(q$chromosome)
+            start <<- min(q$start)
+            end <<- max(q$end)
+        },
+
+        create.ideogram.track = function(...) {
+            ig.track <- IdeogramTrack(genome=genome.ver, chromosome=chromosome, showBandId=TRUE, bevel=1,
+                                      col.axis="black", fontsize=15, fontcolor="black", ...)
+        },
+
+        create.annotation.track = function(...){
+            anno.track <- AnnotationTrack(range=interest, name="miRBase", group=interest$symbol,
+                                           showId=TRUE,chromosome=chromosome, genome=genome.ver,
+                                           fill="orange", col="orange", shape="smallArrow",
+                                           col.axis="black", background.title=NA, col.title="black",
+                                           fontcolor.group="black", fontsize=15)
+        },
+        
+        create.seq.track = function(...) {
+        },
+
+        create.axis.track = function(...) {
+        },
+
+        create.data.track = function(file.dir, ...) {
+        },
+
+        plot = function(...) {
+            plotTracks(list(...), from=start+10, to=end+10)
+        }
+    )
+)
+
+cligb <- Cligb$new(genome.ver="hg19")
+mir.db <- cligb$generate.db(file="~/Dropbox/Desktop.osx/ADAR_paper/data/miRBase.Gviz")
+cligb$find.mirs(db=mir.db, "hsa-mir-22")
+ann.track <- cligb$create.annotation.track()
+print(ann.track)
+#id.track <- cligb$create.ideogram.track()
+cligb$plot(ann.track)
+
+#cligb$plot(id.track)
+
+
+
+
+stop("\n---OK---\n")
+
+
+
 
 generate.bam.tracks <- function(bam.dir, gen, chrm, start, end) {
   bam.files <- list.files(bam.dir, "*sorted.bam$")
